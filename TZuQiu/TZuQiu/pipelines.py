4d8f7542f3fbbf592302717fb346e7abe907a039
wangshiz <wangshiz@users.noreply.github.com>
2018-11-07 22:18:56 +0800

    提交到mysql数据库

--- TZuQiu/TZuQiu/pipelines.py
+++ TZuQiu/TZuQiu/pipelines.py
@@ -4,23 +4,36 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
 
 
 class TzuqiuPipeline(object):
+    def __init__(self):
+        self.db = pymysql.connect(
+            "",
+            "",
+            ""
+        )
+        self.course = self.db.cursor()
 
     def process_item(self, item, spider):
-        print(spider.name)
-        print(item["data"][0].get("playerName"))
+
         for i in range(0, len(item["data"])):
-            pass
-            # print(item["data"][i])
-            # print(item["data"][i].get("playerMainPosition"))
-            # print(item["data"][i].get("leagueName"))
-            # print(item["data"][i].get("playerName"))
-            # print(item["data"][i].get("currentMarketValue"))
-            # print(item["data"][i].get("currentClubName"))
-            # print(item["data"][i].get("mins"))
-            # print(item["data"][i].get("age"))
-            # print(item["data"][i].get("rate"))
-            # print(item["data"][i].get("lastMarketValue"))
+            sql = "insert into player_price values (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            lst = [
+                item["data"][i].get("playerName"),
+                item["data"][i].get("age"),
+                item["data"][i].get("playerMainPosition"),
+                item["data"][i].get("leagueName"),
+                item["data"][i].get("currentClubName"),
+                item["data"][i].get("mins"),
+                item["data"][i].get("rate"),
+                item["data"][i].get("currentMarketValue"),
+                item["data"][i].get("lastMarketValue")
+            ]
+            self.course.execute(sql, lst)
+        self.db.commit()
         return item
+
+    def close_spider(self, spider):
+        self.db.close()
